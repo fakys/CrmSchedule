e160cef151bd5d42ad2ed6505f342f12a64bb331
fakys <fakysgg@gmail.com>
2024-10-31 21:19:57 +0300

    создал метод getFullModule

--- app/Modules/Crm/backend_module/routes/web.php
+++ app/Modules/Crm/backend_module/routes/web.php
@@ -2,6 +2,6 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    dd(\App\Src\BackendHelper::getModule('backend_module')::getNameModule());
+    dd(\App\Src\BackendHelper::getFullModule());
     return 1;
 });
